5c4960cb1c1a71336bc9fd76edc54146cd5c0bd8
Yuan Heng Fan <yuanfan@nbnco.com.au>
2013-03-26 14:28:09 +1100

    Init commit

---
+++ EasyWizard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EasyWizard
+//
+//  Created by Yuan Heng Fan on 03/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EasyWizard/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  EasyWizard
+//
+//  Created by Yuan Heng Fan on 03/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
