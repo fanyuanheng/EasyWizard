c0ec238d58ebe473dd7a086ac2e8af58ae98cc5c
Yuan Heng Fan <yuanfan@nbnco.com.au>
2013-03-26 14:40:05 +1100

    Refactored project structure

---
+++ EasyWizard/EasyWizardViewController.h
@@ -0,0 +1,13 @@
+//
+//  EasyWizardViewController.h
+//  EasyWizard
+//
+//  Created by Yuan Heng Fan on 26/03/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EasyWizardViewController : UIViewController
+
+@end
---
+++ EasyWizard/EasyWizardViewController.m
@@ -0,0 +1,38 @@
+//
+//  EasyWizardViewController.m
+//  EasyWizard
+//
+//  Created by Yuan Heng Fan on 26/03/13.
+//
+//
+
+#import "EasyWizardViewController.h"
+
+@interface EasyWizardViewController ()
+
+@end
+
+@implementation EasyWizardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
